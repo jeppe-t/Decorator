709545bb3de77a814306da3b05dac93a6bf4442a
Jeppe <82437282+jeppe-t@users.noreply.github.com>
2023-01-10 09:15:51 +0100

    decorator script

---
+++ decorator.py
@@ -0,0 +1,39 @@
+#Decorator
+
+"""
+
+A decorator is something we use to add new functionality to existing code without modifying the original code and
+thereby the name decorator. We just decorate the function. In Python, we work with first class functions,
+which gives us the opportunitie to do the implementation as shown below.
+
+This is a basic example of a generator.
+
+I have the existing function siesta() and I made a decorator timer() the can measure the execution time for my function.
+
+"""
+
+from colorama import Fore, Style
+import time
+from time import sleep
+
+#Decorators
+
+print(Fore.GREEN + "\n1. Decorators:" + Style.RESET_ALL)
+
+#Eksempel
+def timer(func):
+    def inner():
+        start = time.time()
+        sleep(1)
+        func()
+        sleep(1)
+        end = time.time()
+        print("Total execution time: ", func.__name__, end - start)
+    return inner
+
+
+@timer
+def siesta():
+    print("Lets do siesta")
+
+siesta()
